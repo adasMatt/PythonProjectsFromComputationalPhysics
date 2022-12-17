b044cbf2e8548172bd14c83a778b1aeaea4a7e55
Matt <madas@hawk.iit.edu>
2022-12-16 22:28:56 -0600

    volume of a cube in a GUI

--- volumeCubeClass.py
+++ volumeCubeClass.py
@@ -16,27 +16,26 @@ from tkinter import ttk
 class App(Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack()
+        self.grid()
 
-        self.entrythingy = Entry()
-        self.entrythingy.pack()
-
-        # to use textvariable in a Label, the variable needs to be first declared and THEN set to a value. Cannot set the value within DoubleVar() such as DoubleVar("8.0").
-        self.defaultvol = DoubleVar()
-        self.defaultvol.set(8.0)
-        
-        #self.outputthingy = Label(root, textvariable=self.defaultvol)
-        self.outputthingy = Label(textvariable=self.defaultvol)     # did not need root here
-        self.outputthingy.pack()
-        self.outputthingy["textvariable"] = 8.0
-
-        self.cubed = DoubleVar()
-        self.cubed.set(3.0)
-
-        # Create the cube length variable.
-        self.length_input = DoubleVar()
+        # making tkinter variables
+        #self.volume = DoubleVar()
+        self.volume = StringVar()
+        #meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        self.length_input = StringVar()
         # Set it to some default value.
         self.length_input.set(2.0)
+
+        # making widgets
+        self.lengthEntry = Label(text="Enter side length: ").grid(column=0,row=0)
+        self.entrythingy = Entry()
+        self.entrythingy.grid(column=1,row=0)
+        
+        self.volumeCalculated = Label(text="Volume of this cube is: ").grid(column=0,row=1)
+        self.labelthingy = Label(textvariable=self.volume)
+        self.labelthingy.grid(column=1,row=1)
+        self.buttonthingy = ttk.Button(text="Compute", command=self.button_volume_compute)
+        self.buttonthingy.grid(column=1,row=2)
         
         # Tell the entry widget to watch this variable.
         self.entrythingy["textvariable"] = self.length_input        
@@ -45,29 +44,26 @@ class App(Frame):
         # It prints the current value of the variable.
         self.entrythingy.bind('<Key-Return>',
                              self.volume_compute)
-
-        #######################################################
-        #first things first I have a button, this is great. However I would like clicking the button to do the same as pressing return key for now.
-        # Next I would like this to print in a separate widget instead of being printed to terminal
-        self.buttonthingy = ttk.Button(text="Compute", command=self.button_volume_compute)
-        self.buttonthingy.pack()
         
-    # Rewritten from volume_compute used in Entry but with *args instead of event as the argument
-    def button_volume_compute(self, *args):
-
-        length = self.length_input.get()
-        volume = length**self.cubed.get()
-        print("The volume of a cube with sides of length", length, "is:", volume)
-
-    # this prints result to terminal
     def volume_compute(self, event):
-        
-        length = self.length_input.get()
-        volume = length**self.cubed.get()
-        print("The volume of a cube with sides of length", length, "is:", volume)
-        
+        #this just prevents errors from string inputs instead of numbers
+        try:
+            length = self.length_input.get()
+            self.volume.set(int(float(length)**3.0 * 10000)/10000.0)        
+        except ValueError:
+            pass
+    
+    # Rewritten from volume_compute above but with *args instead of event as the argument
+    def button_volume_compute(self, *args):
+        try:
+            length = self.length_input.get()
+            self.volume.set(int(float(length)**3.0 * 10000)/10000.0)        
+        except ValueError:
+            pass
+
 
 
 root = Tk()
+root.title("Volume of Cube")
 myapp = App(root)
 myapp.mainloop()
